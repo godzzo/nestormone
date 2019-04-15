472c664331a8ef68b1f023e6bf26034aac0f561e
godzzolp <godzzo@lightport.hu>
2019-04-15 21:14:38 +0200

    Testing json post request body - it is just works
    testing with postman

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
+
+	@Post('testBody')
+	testBody(@Body() obj: any): any {
+		console.log(JSON.stringify(obj));
+
+		return obj;
+	}
 }
